2e5c6706c083eac0a58ddb1d67570416bc8efcb8
kbeck473 <kbeck473@gmail.com>
2025-04-13 23:45:24 -0700

    Greeting component
    Created component for greetig page and added  button placeholder

---
+++ my-app/src/app/components/greeting.jsx
@@ -0,0 +1,18 @@
+function Greeting() {
+    return (
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content text-center">
+          <div className="max-w-md">
+            <h1 className="text-5xl font-bold">Hello there!</h1>
+            <p className="py-6">
+              I am Kyle Beck, a software engineer with a passion for creating innovative solutions. Welcome to my portfolio!
+            </p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  
+  export default Greeting;
+  
--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -2,6 +2,7 @@
 
 import Header from "./components/Header";
 import ProjectPage from "./components/projects/ProjectPage";
+import Greeting from "./components/Greeting";
 import "./globals.css";
 
 export default function Home() {
@@ -11,10 +12,8 @@ export default function Home() {
       {/* Adding mt-8 adds margin-top between the header and the portfolio section */}
       <main className="container mx-auto mt-8 py-10 text-center">
         <div className="flex flex-col gap-6">
-          <h1 className="text-4xl font-bold">My Portfolio</h1>
-          <p className="text-lg text-gray-600">
-            Welcome to my portfolio! Here you will find my projects and skills.
-          </p>
+          <Greeting />
+          
           <ProjectPage />
         </div>
       </main>
