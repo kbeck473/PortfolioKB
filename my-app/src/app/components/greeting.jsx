123ec422c4a158b78fa3c329de245ccf894d1db8
kbeck473 <kbeck473@gmail.com>
2025-04-14 11:30:28 -0700

    Test for CI
    Changed text for CI test
    also removed blank greeting file

--- my-app/src/app/components/greeting.jsx
+++ my-app/src/app/components/greeting.jsx
@@ -3,7 +3,7 @@ function Greeting() {
       <div className="hero min-h-screen bg-base-300">
         <div className="hero-content text-center">
           <div className="max-w-md">
-            <h1 className="text-5xl font-bold">Hello there!</h1>
+            <h1 className="text-5xl font-bold">Hello there! Test For CI</h1>
             <p className="py-6">
               I am Kyle Beck, I am a developer with a education in Software Engineering and a Professional background in IT.  Welcome to my portfolio!
             </p>
