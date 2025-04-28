ba198f59e846a336a7b6c5887d9842cd0d57e8d1
kbeck473 <kbeck473@gmail.com>
2025-04-27 22:19:30 -0700

    fixed import scheme

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,7 +1,7 @@
 // app/page.tsx or pages/index.tsx
 
 import Header from "./components/Header";
-import ProjectPage from "./components/projects/ProjectPage";
+import ProjectsPage from "./components/projects/ProjectPage";
 import Greeting from "./components/greeting";
 import Footer from "./components/Footer"
 import "./globals.css";
@@ -17,7 +17,7 @@ export default function Home() {
       <main className="container mx-auto mt-8 py-10 text-center">
         <div className="flex flex-col gap-6 bg-">
           <ExperiencesPage/>
-          <ProjectPage />
+          <ProjectsPage />
         </div>
       </main>
 
