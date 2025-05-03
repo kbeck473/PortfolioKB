dc7eeb8576acbac84870bf9fddd3dbf6e9617232
kbeck473 <kbeck473@gmail.com>
2025-05-02 19:50:24 -0700

    Remove theme panel

--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import "@radix-ui/themes/styles.css";
-import { Theme, ThemePanel} from "@radix-ui/themes";
+import { Theme, } from "@radix-ui/themes";
 
 
 const geistSans = Geist({
@@ -32,7 +32,6 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
      <Theme appearance="dark" accentColor="blue" radius="large">
       {children}
-          <ThemePanel/>
         </Theme>
       </body>
     </html>
